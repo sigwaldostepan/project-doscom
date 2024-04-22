b98b01ace715e331d656a75c5c707a0f9f329398
unknown <sigwaldos@gmail.com>
2024-04-23 00:37:33 +0700

    add react router dom for navigation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import Navbar from "./components/Navbar/index.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // children: [
+
+    // ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={routes} />
+  </React.StrictMode>
+);
