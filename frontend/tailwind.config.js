4801a71c4d7d5ac5f95ec3390d79059ec18c8bf8
unknown <sigwaldos@gmail.com>
2024-04-23 00:45:41 +0700

    add daisyui

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  daisyui: {
+    themes: [
+      {
+        lightMode: {
+          primary: "#a855f7",
+          secondary: "#9333ea",
+          accent: "#6d28d9",
+          neutral: "#111827",
+          "base-100": "#f3f4f6",
+          info: "#ff00ff",
+          success: "#22c55e",
+          warning: "#facc15",
+          error: "#ef4444",
+        },
+        darkMode: {
+          primary: "#9333ea",
+          secondary: "#7e22ce",
+          accent: "#00ffff",
+          neutral: "#7c3aed",
+          "base-100": "#252525",
+          info: "#7e22ce",
+          success: "#eab308",
+          warning: "#00ff00",
+          error: "#dc2626",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
