c76b6030aee74c53d741c69b1b1cc757888a270f
sigwaldoss <sigwaldos@gmail.com>
2024-04-23 09:19:26 +0700

    update react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,46 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Outlet,
-} from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
-import Navbar from "./components/Navbar/index.jsx";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-
-const Layout = () => {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
-};
-
-const routes = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    // children: [
-
-    // ],
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-]);
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
+    <App />
   </React.StrictMode>
 );
