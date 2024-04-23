66210756b4d9057b527bd8908e0eb5d368ba2195
sigwaldoss <sigwaldos@gmail.com>
2024-04-23 09:42:17 +0700

    update hero container

--- frontend/src/components/HeroContainer/index.jsx
+++ frontend/src/components/HeroContainer/index.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import Button from "../Button";
 
-const HeroContainer = ({ img, title, description }) => {
+const HeroContent = ({ img, title, description }) => {
   return (
-    <div className="hero-content max-w-[900px] flex-col gap-12 transition-all lg:flex-row hover:-translate-y-1 hover:shadow-lg even:flex-row-reverse">
+    <div className="hero-content max-w-[900px] flex-col gap-12 rounded-lg transition-all lg:flex-row hover:-translate-y-1 hover:shadow-lg even:flex-row-reverse">
       <img
         src={img}
-        className="min-h-[300px] max-h-[300px] aspect-square object-cover rounded-lg shadow-3xl"
+        className="min-h-[300px] max-h-[300px] aspect-square object-cover rounded-md shadow-3xl"
       />
       <div>
         <h1 className="text-5xl font-bold">{title}</h1>
         <p className="py-6">{description}</p>
-        <button className="btn btn-primary">Read more</button>
+        <Button type={"button"} message={"Read more"} />
       </div>
     </div>
   );
 };
 
-export default HeroContainer;
+export default HeroContent;
