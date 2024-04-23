f118a9fb9e479dadff3575363d03282cd9388f2c
sigwaldoss <sigwaldos@gmail.com>
2024-04-23 11:46:06 +0700

    fixed features and bugs

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -3,10 +3,9 @@ import ThemeController from "../ThemeController";
 const Navbar = () => {
   return (
     <>
-      <nav className="sticky top-0 left-0 p-4 bg-transparent backdrop-blur-[10px]">
-        <div>
+      <nav className="sticky w-full top-0 left-0 z-[999] px-6 py-4 bg-transparent shadow backdrop-blur-[10px]">
+        <div className="flex items-center justify-between">
           <h1>Logo</h1>
-
           <div>
             <ThemeController />
           </div>
