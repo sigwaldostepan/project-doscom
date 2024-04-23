b192f033ed0ea8f487e326092b604cf90044de82
sigwaldoss <sigwaldos@gmail.com>
2024-04-23 11:47:13 +0700

    adjust size

--- frontend/src/components/ThemeController/index.jsx
+++ frontend/src/components/ThemeController/index.jsx
@@ -20,7 +20,7 @@ const ThemeController = () => {
           onClick={handleTheme}
         />
         <svg
-          className="swap-off fill-current w-10 h-10"
+          className="swap-off fill-current w-6 h-6"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
         >
@@ -28,7 +28,7 @@ const ThemeController = () => {
         </svg>
 
         <svg
-          className="swap-on fill-current w-10 h-10"
+          className="swap-on fill-current w-6 h-6"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
         >
