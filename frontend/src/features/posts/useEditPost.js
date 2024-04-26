d296247489387193a42d0b9a5e502009aa55c5ad
sigwaldoss <sigwaldos@gmail.com>
2024-04-26 17:31:53 +0700

    commit

--- frontend/src/features/posts/useEditPost.js
+++ frontend/src/features/posts/useEditPost.js
@@ -4,7 +4,6 @@ import { axiosInstance } from "../../lib/axiosInstance";
 const useEditPost = (id) => {
   return useMutation({
     mutationFn: async ({ title, desc, tags, img, date, username }) => {
-      console.log(title);
       return await axiosInstance.put(`/posts/${id}`, {
         title,
         desc,
